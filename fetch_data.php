1c75fc3554b8e5cd013b4bc332747cd5e2c0a548
Mohd Shahril <mohd_shahril_96@yahoo.com>
2015-12-30 23:50:49 +0800

    automatic convert post value to uppercase

--- fetch_data.php
+++ fetch_data.php
@@ -5,7 +5,7 @@ if(!empty($_GET['get_option'])) {
 	$json = json_decode(file_get_contents("zon.json"), true);
 	$html = "";
 
-	foreach($json[$_GET['get_option']] as $kod => $kawasan) {
+	foreach($json[strtoupper($_GET['get_option'])] as $kod => $kawasan) {
 		$html .= "<option value='{$kod}'>{$kawasan}</option>\n";
 	}
 
