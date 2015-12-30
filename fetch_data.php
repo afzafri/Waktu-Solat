d44f87c98ed4556abca133c058de07f4d970f9a3
Mohd Shahril <mohd_shahril_96@yahoo.com>
2015-12-30 23:00:14 +0800

    forget to put close square bracket

--- fetch_data.php
+++ fetch_data.php
@@ -5,7 +5,7 @@ if(!empty($_POST['get_option'])) {
 	$json = json_decode(file_get_contents("zon.json"), true);
 	$html = "";
 
-	foreach($json[$_POST['get_option'] as $kod => $kawasan]) {
+	foreach($json[$_POST['get_option']] as $kod => $kawasan) {
 		$html .= "<option value='{$kod}'>{$kawasan}</option>\n";
 	}
 
